82556916e28ece387620de688a8bb526dc713c43
HaiBang1010 <haibang1010999@gmail.com>
2024-08-15 23:35:08 +0700

    feat (login page)

--- src/app/modules/authentication/login/login.module.ts
+++ src/app/modules/authentication/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule, Routes } from '@angular/router';
-
+import { ReactiveFormsModule } from '@angular/forms';
 const routes: Routes = [
   {
     path: '',
@@ -11,7 +11,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
+  // imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: [LoginComponent],
 })
 export class LoginModule {}
