c4b33d686400262e81c6c1cd00bf23160eb52410
HaiBang1010 <haibang1010999@gmail.com>
2024-08-16 13:16:11 +0700

    feat (reset password): create UI

--- src/app/modules/authentication/reset-password/reset-password.component.ts
+++ src/app/modules/authentication/reset-password/reset-password.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.css']
+  styleUrls: ['./reset-password.component.css'],
 })
 export class ResetPasswordComponent implements OnInit {
+  constructor(private router: Router) {}
+  ngOnInit() {}
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  get f() {
+    return this.form.controls;
   }
 
+  submit() {
+    console.log(this.f);
+    // this.authService.login(this.loginPayload).subscribe((res) => {
+    //   console.log(res);
+    // });
+    // this.router.navigate(["/dashboards/dashboard"]);
+  }
 }
--- src/app/modules/authentication/reset-password/reset-password.module.ts
+++ src/app/modules/authentication/reset-password/reset-password.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResetPasswordComponent } from './reset-password.component';
 import { RouterModule, Routes } from '@angular/router';
-
+import { ReactiveFormsModule } from '@angular/forms';
 const routes: Routes = [
   {
     path: '',
@@ -11,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
   declarations: [ResetPasswordComponent],
 })
 export class ResetPasswordModule {}
