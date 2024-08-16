222a522f77448c992da417c8a0995a7d4ce7abb0
HaiBang1010 <haibang1010999@gmail.com>
2024-08-16 13:41:07 +0700

    feat (change password): create UI

--- src/app/modules/authentication/change-password/change-password.component.ts
+++ src/app/modules/authentication/change-password/change-password.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.css']
+  styleUrls: ['./change-password.component.css'],
 })
 export class ChangePasswordComponent implements OnInit {
+  constructor(private router: Router) {}
+  ngOnInit() {}
+  form: FormGroup = new FormGroup({
+    newPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  get f() {
+    return this.form.controls;
   }
 
+  submit() {
+    console.log(this.f);
+    // this.authService.login(this.loginPayload).subscribe((res) => {
+    //   console.log(res);
+    // });
+    // this.router.navigate(["/dashboards/dashboard"]);
+  }
 }
